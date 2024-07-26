d97bc77716369a7bff1ccbcff4d36c327197ebf1
Vasu Garg <vasugarg924@gmail.com>
2024-07-26 22:38:16 +0530

    updating network port and some minor changes

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -15,7 +15,7 @@ function Freebook() {
     useEffect(()=>{
         const getBook=async()=>{
             try{
-                const res=await axios.get("http://localhost:4001/book");
+                const res=await axios.get("https://book-store-backend-ujwy.onrender.com/book");
                 // console.log(res.data);
                 setBook(res.data.filter((data)=>data.category==="Free"))
             } catch(error){
--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -16,7 +16,7 @@ function Signup() {
             email:data.email,
             password:data.password,
         }
-        await axios.post("http://localhost:4001/user/signup",userInfo)
+        await axios.post("https://book-store-backend-ujwy.onrender.com/user/signup",userInfo)
         .then((res)=>{
             console.log(res.data)
             if(res.data){
